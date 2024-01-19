7621ec581be8166e35994801a1bd5427c660dab6
Techbite <bonfacemwema7@gmail.com>
2024-01-19 14:02:45 +0300

    integrated static and templates successfully

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,8 @@ from myapp import views
 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("/Register", views.register, name="register"),
+    path("/login", views.login, name="login"),
+    path("/logout", views.logout, name="logout"),
 ]
